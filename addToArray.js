08dd5ff71e459d53911efee6f0a20d6973005d60
professeur01 <124563602+professeur01@users.noreply.github.com>
2023-02-11 23:42:43 +0100

    Update addToArray.js

--- addToArray.js
+++ addToArray.js
@@ -1,4 +1,4 @@
-const  myArray = [["John", 23] , ["cat", 2]];
-myArray.push (["Cs", 1]);
+const  myArray = ['["John", 23] "," ["cat", 2]'];
+myArray.push ('["Cs", 1]');
 
-    console.log(...myArray);
+    console.log(myArray);
